8cd5fe864997a55f59313a9d1567609ccdc36d9c
Lydia Amadi Chinyere <Lydiaamadichinyere@gmail.com>
2023-04-21 19:26:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+let scoreHome = document.getElementById("score-home");
+let scoreGuest = document.getElementById("score-guest");
+
+let homeScore = 0;
+let guestScore = 0;
+
+function homePlusOne() {
+  homeScore += 1;
+  scoreHome.textContent = homeScore;
+}
+
+function homePlusTwo() {
+  homeScore += 2;
+  scoreHome.textContent = homeScore;
+}
+
+function homePlusThree() {
+  homeScore += 3;
+  scoreHome.textContent = homeScore;
+}
+
+function guestplusOne() {
+  guestScore += 1;
+  scoreGuest.textContent = guestScore;
+}
+
+function guestplusTwo() {
+  guestScore += 2;
+  scoreGuest.textContent = guestScore;
+}
+function guestplusThree() {
+  guestScore += 3;
+  scoreGuest.textContent = guestScore;
+}
